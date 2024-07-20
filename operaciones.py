352af38c1f2ad81b48ea4a7e8ff350c078289917
olga <olgamacias.r@gmail.com>
2024-07-20 21:00:59 +0200

    pruebas operaciones

---
+++ operaciones.py
@@ -0,0 +1,23 @@
+
+def sumar(a, b):
+    return a + b
+
+
+def restar(a, b):
+    return a - b
+
+
+def multiplicar(a, b):
+    return a * b
+
+
+def dividir(a, b):
+    if b == 0:
+        raise ZeroDivisionError("No se puede dividir entre 0.")
+    return a / b    
+
+if __name__ == "__main__":
+    print(sumar(5, 3))
+    print(restar(5, 3))
+    print(multiplicar(5, 3))
+    print(dividir(5, 3))
---
+++ test_dividir.py
@@ -0,0 +1,14 @@
+import unittest
+from operaciones import dividir
+
+class TestDividir(unittest.TestCase):
+
+    def test_dividir(self):
+        self.assertEqual(dividir(6, 2), 3)
+        self.assertEqual(dividir(-1, 1), -1)
+        self.assertEqual(dividir(-1, -1), 1)
+        with self.assertRaises(ZeroDivisionError):
+            dividir(5, 0)
+        
+if __name__ == '__main__':
+ unittest.main()
